f1350ad99820c579467b0e9f3cffa38cb68850cc
Prexhaj <126613596+Prexhaj@users.noreply.github.com>
2023-03-30 21:10:13 +0200

    updated bucket acl

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "aws_s3_bucket" "bucket" {
     Environment = var.environment
   }
 }
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
